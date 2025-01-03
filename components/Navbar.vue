94dc46c52bb46d00902099c6fff7a0110b1b824e
kashifal <captainkashi4@gmail.com>
2025-01-03 22:32:51 +0500

    navbar - Pages

---
+++ components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="h-24 w-full bg-white px-6 w-full flex justify-between items-center shadow-sm">
+        <div class="flex items-center gap-2">
+            
+            <h1 class="text-2xl font-bold">Logo</h1>
+        </div>
+    </div>
+</template>
+
+ 
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="">
+        <Navbar />
+        <div class="p-6">
+            Index
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
